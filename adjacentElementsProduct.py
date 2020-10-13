8bed88b0c82d4bdcc0b85bd1774b655d098e6909
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2020-10-13 16:45:40 +0100

    Delete adjacentElementsProduct.py

--- adjacentElementsProduct.py
+++
@@ -1,6 +0,0 @@
-def adjacentElementsProduct(inputArray):
-    ArrayEnd = []
-    for i in range(len(inputArray) - 1):
-        ArrayEnd.append(inputArray[i]*inputArray[i+1])
-    maximum = max(ArrayEnd)
-    return maximum
