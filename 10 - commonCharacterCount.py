c561d46901f321655a7ce087db5700ba2f9c360d
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2020-10-18 21:57:37 +0100

    Add files via upload

---
+++ 10 - commonCharacterCount.py
@@ -0,0 +1,10 @@
+def commonCharacterCount(s1, s2):
+    s1_l = list(s1)
+    s2_l = list(s2)
+    common = []
+
+    for i in s1_l:
+        if i in s2_l:
+            common.append(i)
+            s2_l.remove(i)
+    return len(common)
