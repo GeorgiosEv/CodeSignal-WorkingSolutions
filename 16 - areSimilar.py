8e09abcdc8795e8b5577c8e786525a5aecc06d43
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2020-10-29 04:37:09 +0000

    Update 16 - areSimilar.py

--- 16 - areSimilar.py
+++ 16 - areSimilar.py
@@ -2,7 +2,8 @@ def areSimilar(a, b):
     check_a = []
     check_b = []
     count = 0
-    for i in range(len(a)):
+    for i in range(len(a)): 
+    #The code works perfectly fine if you swap len(a) with len(b) (checked)
         if a[i] != b[i]:
             count += 1
             check_a.append(a[i])
