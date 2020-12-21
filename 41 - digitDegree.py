99a7f6c453e37ee5a18d5c02cafa9ce7952ad8ba
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2020-12-21 06:10:49 +0000

    Update 41 - digitDegree.py

--- 41 - digitDegree.py
+++ 41 - digitDegree.py
@@ -1,6 +1,7 @@
 def digitDegree(n):
     degree = 0
     while 10 <= n:
-        n = sum(int(i) for i in str(n))
+        num = str(n)
+        n = sum(int(i) for i in num)
         degree += 1
     return degree
