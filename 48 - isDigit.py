ac6b698e92461cd4aa3c45b6ced57683531b2c56
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2020-12-29 13:48:08 +0000

    Add files via upload

---
+++ 48 - isDigit.py
@@ -0,0 +1,9 @@
+# Solution 1: Analytic
+def isDigit(symbol):
+    if symbol.isdigit():
+        return True
+    return False
+
+# Solution 2: One-liner
+def isDigit(symbol):
+    return symbol.isdigit()
