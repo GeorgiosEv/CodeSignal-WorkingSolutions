efbcc65a6d0eaf20c4aa439f453b54319ada52dc
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2020-12-29 13:19:35 +0000

    Update 46 - electionWinners.py

--- 46 - electionWinners.py
+++ 46 - electionWinners.py
@@ -3,4 +3,4 @@ def electionsWinners(votes, k):
     len_vote = len(votes)
     if k == 0 and votes.count(max_vote) == 1:
         return 1
-    return len([ i for i in range(len_vote) if votes[i] + k > max_vote])
+    return len([ i for i in range(len_vote) if votes[i] + k > max_vote ])
