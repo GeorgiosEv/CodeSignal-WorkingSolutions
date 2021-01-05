854a0c639b982247f1149002eeb411e665245e0e
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 20:43:22 +0000

    Update 03 - checkPalindrome.py

--- Codesignal Arcade - Intro/03 - checkPalindrome.py
+++ Codesignal Arcade - Intro/03 - checkPalindrome.py
@@ -1,7 +1,20 @@
+"""Given the string, check if it is a palindrome.
+
+Example:
+- For inputString = "aabaa", the output should be checkPalindrome(inputString) = true;
+- For inputString = "abac", the output should be checkPalindrome(inputString) = false;
+- For inputString = "a", the output should be checkPalindrome(inputString) = true."""
 def checkPalindrome(inputString):
+    # Step 1: We turn all letters in our given string into lower case.
     inp = inputString.lower()
+    # Step 2: Also, we discard all space between words or letters.
+    # It is important to store the result in the same variable as in step 1.
+    # Think of it as just only dealing with discarding the spaces.
     inp = inp.replace(" ","")
+    # The first two steps help us end up with a long, uniform (in terms of letter size) string.
+    # We define a variable called "reverse" as the reversed counterpart of the input string.
     reverse = inp[::-1]
+    # The outcome will be "True" only if the original string and its reversed counterpart are the same.
     if (reverse == inp):
         return True
     else:
