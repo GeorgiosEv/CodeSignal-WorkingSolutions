a506e6d64f8a56b9c769b17d4bc1da5071e4ac12
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 16:27:50 +0000

    Delete 03 - checkPalindrome.py

--- 03 - checkPalindrome.py
+++
@@ -1,8 +0,0 @@
-def checkPalindrome(inputString):
-    inp = inputString.lower()
-    inp = inp.replace(" ","")
-    reverse = inp[::-1]
-    if (reverse == inp):
-        return True
-    else:
-        return False
