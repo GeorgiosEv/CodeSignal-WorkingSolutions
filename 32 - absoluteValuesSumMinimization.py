a0e7c6f7efa8e43020d665bdbe61c9b794958f5e
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 16:57:55 +0000

    Delete 32 - absoluteValuesSumMinimization.py

--- 32 - absoluteValuesSumMinimization.py
+++
@@ -1,8 +0,0 @@
-def absoluteValuesSumMinimization(a):
-    sums = []
-    for i in range(len(a)):
-        sum = 0
-        for j in range(len(a)):
-            sum += abs(a[i] - a[j])
-        sums.append(sum)
-    return a[sums.index(min(sums))]
