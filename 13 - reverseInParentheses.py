4dea12982649c057df8a9baf46c205bbd2c6687f
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 16:46:10 +0000

    Delete 13 - reverseInParentheses.py

--- 13 - reverseInParentheses.py
+++
@@ -1,8 +0,0 @@
-def reverseInParentheses(inputString):
-    for i in range(len(inputString)):
-        if inputString[i] == "(":
-            start = i
-        if inputString[i] == ")":
-            end = i
-            return reverseInParentheses(inputString[:start] + inputString[start+1:end][::-1] + inputString[end+1:])
-    return inputString
