bf3722b9079724371258e60398b76b58b92cb044
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 16:58:24 +0000

    Delete 34 - extractEachKth.py

--- 34 - extractEachKth.py
+++
@@ -1,8 +0,0 @@
-def extractEachKth(inputArray, k):
-    inp = []
-    for i in range(len(inputArray)):
-        if (i+1)%k == 0:
-            pass
-        else:
-            inp.append(inputArray[i])
-    return inp
