22995cf9c19e510ba929b911a2d557535f6d1d88
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 19:38:09 +0000

    Update 01 - add.py

--- Codesignal Arcade - Intro/01 - add.py
+++ Codesignal Arcade - Intro/01 - add.py
@@ -1,3 +1,9 @@
+""" Write a function that returns the sum of two numbers.
+
+Example:
+For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3. """
+
 def add(param1, param2):
+    # A pretty straightforward exercise. A function including 2 parameters, which are summed together, returning the sum as the result.
     sum = param1 + param2
     return sum
