6d21a22eb5032686a7b40bf88c67bf4e4126cea6
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 21:13:59 +0000

    Update 01 - add.py

--- Codesignal Arcade - Intro/01 - add.py
+++ Codesignal Arcade - Intro/01 - add.py
@@ -2,7 +2,6 @@
 
 Example:
 For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3. """
-
 def add(param1, param2):
     # A pretty straightforward exercise. A function including 2 parameters, which are summed together, returning the sum as the result.
     sum = param1 + param2
