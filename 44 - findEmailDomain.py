52ab85e4cb7ffe971343b9af09c6d71748fd637a
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 17:04:12 +0000

    Delete 44 - findEmailDomain.py

--- 44 - findEmailDomain.py
+++
@@ -1,7 +0,0 @@
-def findEmailDomain(address):
-    address_spl = address.split("@")
-    c = [ i for i in address_spl ]
-    if len(address_spl) == 2:
-        return c[1]
-    if len(address_spl) == 3:
-        return c[2]
