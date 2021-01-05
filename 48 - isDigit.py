9a56a8ba23dc2bc672af783b089b742db5ef0f59
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 17:04:53 +0000

    Delete 48 - isDigit.py

--- 48 - isDigit.py
+++
@@ -1,9 +0,0 @@
-# Solution 1: Analytic
-def isDigit(symbol):
-    if symbol.isdigit():
-        return True
-    return False
-
-# Solution 2: One-liner
-def isDigit(symbol):
-    return symbol.isdigit()
