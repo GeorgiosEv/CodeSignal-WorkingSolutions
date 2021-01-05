fd3752c511ffd27a9351a94c394351750f316d0e
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 16:57:46 +0000

    Delete 31 - depositProfit.py

--- 31 - depositProfit.py
+++
@@ -1,6 +0,0 @@
-def depositProfit(deposit, rate, threshold):
-    year_count = 0
-    while deposit < threshold:
-        deposit = deposit + (deposit * (rate/100))
-        year_count += 1
-    return year_count
