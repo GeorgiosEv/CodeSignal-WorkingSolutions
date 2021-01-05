19b8c1f4c67ed003fb93ecbe8d121384fe7c3b2b
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 16:55:12 +0000

    Delete 25 - arrayReplace.py

--- 25 - arrayReplace.py
+++
@@ -1,8 +0,0 @@
-def arrayReplace(inputArray, elemToReplace, substitutionElem):
-    new = []
-    for i in range(len(inputArray)):
-        if inputArray[i] == elemToReplace:
-            new.append(substitutionElem)
-        else:
-            new.append(inputArray[i])
-    return new
