df09e0bac205dd501f8fd500b778ca12c7097087
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 17:04:26 +0000

    Delete 45 - buildPalindrome.py

--- 45 - buildPalindrome.py
+++
@@ -1,7 +0,0 @@
-def buildPalindrome(st):
-    for i in range(len(st)):
-        sub = st[i:len(st)]
-        if sub[::-1] == sub:
-            missing = st[0:i]
-            return st + missing[::-1]
-    return st
