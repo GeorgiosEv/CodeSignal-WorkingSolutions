1fefaac45ebffaef35c30041a463a854077f64db
Georgios Evangelinos <72348058+GeorgiosEv@users.noreply.github.com>
2021-01-05 16:36:05 +0000

    Delete 09 - allLongestStrings.py

--- 09 - allLongestStrings.py
+++
@@ -1,7 +0,0 @@
-def allLongestStrings(inputArray):
-    max_arr = []
-    max_len = len(max(inputArray, key = len))
-    for i in inputArray:
-        if len(i) == max_len:
-            max_arr.append(i)
-    return max_arr
